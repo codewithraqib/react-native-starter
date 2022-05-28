3fd434f76a228724cab810faa491f34e8e8acce2
“Raqib <raqib850@gmail.com>
2022-05-29 01:17:48 +0530

    first commit

---
+++ src/screens/MessagesScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const MessagesScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Messages Screen</Text>
+    </View>
+  );
+};
+
+export default MessagesScreen;
